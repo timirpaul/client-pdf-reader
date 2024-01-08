2d0158ec4a62cc711d69c0a50071bfbd019a9615
Timir <timirpaul57@gmail.com>
2024-01-08 20:37:12 +0530

    add loading

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -12,83 +12,89 @@ const Search = () => {
 
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
+    setLoading(true)
     const res = await axios.get(url);
     setData(res.data);
+    setLoading(false)
   };
 
   useEffect(() => {
     fetchData();
-    
   }, []);
- 
+
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      if(search==="") return fetchData();
-      console.log("search",search);
-      const res = await axios.post(searchUrl, { search  });
-      console.log(res);
+      setLoading(true)
+      if (search === "") return fetchData();
+      // console.log("search", search);
+      const res = await axios.post(searchUrl, { search });
+      // console.log(res);
       setData(res.data.data);
     } catch (error) {
       console.log(error);
-    }
+    }finally{setLoading(false)}
   };
 
-
-  console.log(data);
-  console.log(process.env.REACT_APP_PROXY);
+  // console.log(data);
+  // console.log(process.env.REACT_APP_PROXY);
   // console.log("search",search);
   return (
     <>
-      <div className="container">
-        <div className="card ">
-          <div className="card-body m-3 ">
-            <Link
-              to={"/"}
-              className=" btn btn-primary m-3"
-              style={{ float: "right", width: "200px" }}
-            >
-              Upload CV
-            </Link>
+      {loading ? 
+        <> Loading...</>
+       : 
+        <div className="container">
+          <div className="card ">
+            <div className="card-body m-3 ">
+              <Link
+                to={"/"}
+                className=" btn btn-primary m-3"
+                style={{ float: "right", width: "200px" }}
+              >
+                Upload CV
+              </Link>
 
-            <h1>Search CV</h1>
-            <form onSubmit={submitHandler}>
-              <div className="mb-3">
-                <input
-                  className="form-control"
-                  type="text"
-                  placeholder="search any skill keywords"
-                  id="formFileDisabled"
-                  onChange={(e) => {
-                    setSearch(e.target.value);
-                  }}
-                  //   disabled
-                />
-                <button className=" btn btn-primary m-3" type="submit">
-                  Search
-                </button>
+              <h1>Search CV</h1>
+              <form onSubmit={submitHandler}>
+                <div className="mb-3">
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="search any skill keywords"
+                    id="formFileDisabled"
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                    //   disabled
+                  />
+                  <button className=" btn btn-primary m-3" type="submit">
+                    Search
+                  </button>
+                </div>
+              </form>
+
+              <div className="row">
+                <DeleteAll dataList={data} />
+                {data?.length > 0
+                  ? data?.map((item, i) => (
+                      <div className="col-6 col-md-3 m-3" key={item._id}>
+                        <Card
+                          name={item.name}
+                          filePath={item.file}
+                          email={item.email}
+                        />
+                      </div>
+                    ))
+                  : "No CV Found"}
               </div>
-            </form>
-
-            <div className="row">
-            <DeleteAll dataList={data}/>
-              {data?.length > 0
-                ? data?.map((item, i) => (
-                    <div className="col-6 col-md-3 m-3" key={item._id} >
-                      <Card
-                        name={item.name}
-                        filePath={item.file}
-                        email={item.email}
-                      />
-                    </div>
-                  ))
-                : "No CV Found"}
             </div>
           </div>
         </div>
-      </div>
+      }
     </>
   );
 };
